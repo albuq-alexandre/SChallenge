41f7f6377c0a71d5e2f7f7b0908e151a406006f9
albuq-alexandre <albuq.alexandre@gmail.com>
2017-10-03 20:56:22 -0300

    Initial commit

---
+++ app/src/main/java/br/iesb/a1631088056/schallenge/activities/MainActivity.java
@@ -0,0 +1,15 @@
+package br.iesb.a1631088056.schallenge.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import br.iesb.a1631088056.schallenge.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
