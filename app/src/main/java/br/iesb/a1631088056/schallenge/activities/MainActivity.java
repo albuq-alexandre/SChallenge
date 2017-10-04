425733d25d349dc30125561f2a0afa70d58a5b38
albuq-alexandre <albuq.alexandre@gmail.com>
2017-10-03 22:14:53 -0300

    second commit

--- app/src/main/java/br/iesb/a1631088056/schallenge/activities/MainActivity.java
+++ app/src/main/java/br/iesb/a1631088056/schallenge/activities/MainActivity.java
@@ -2,6 +2,7 @@ package br.iesb.a1631088056.schallenge.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import br.iesb.a1631088056.schallenge.R;
 
@@ -12,4 +13,26 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Toast.makeText(this, "Passando pelo onStart...", Toast.LENGTH_LONG);
+        
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+
+
 }
+
+
---
+++ app/src/main/java/br/iesb/a1631088056/schallenge/activities/telaCadastro.java
@@ -0,0 +1,15 @@
+package br.iesb.a1631088056.schallenge.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import br.iesb.a1631088056.schallenge.R;
+
+public class telaCadastro extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tela_cadastro);
+    }
+}
